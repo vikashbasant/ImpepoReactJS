6d7491b8c57067f010093409e711aa8a0ce893fd
Vikash Basant <basantvikash360@gmail.com>
2023-06-08 16:29:35 +0530

    impepo project ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+const youtuber = "thapa techincal";
+
+const favprog = "React js";
+
+function myName() {
+  let name = "vikash";
+  return name;
+}
+
+function myLast() {
+  let last = "vikko";
+  return last;
+}
+
+export default youtuber;
+
+export { favprog, myName, myLast };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import youtuber from "./App";
+// import youtuber, { favprog, myName, myLast } from "./App";
+
+// both same: default ka naam kuch bhi rkah skta hun.
+
+// Recomended:
+import myFav, { favprog, myName, myLast } from "./App";
+
+// not recomended:
+// import * as kash from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  // one way:
+  <>
+    <ol>
+      <li> vikash </li>
+      {/* <li> {youtuber} </li> */}
+      <li> {myFav} </li>
+      <li> {favprog} </li>
+      <li> {myName()} </li>
+      <li> {myLast()} </li>
+    </ol>
+  </>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // <>
+  //   <ol>
+  //     <li> Vikash </li>
+  //     <li> {kash.default} </li>
+  //     <li> {kash.favprog} </li>
+  //     <li> {kash.myName()} </li>
+  //     <li> {kash.myLast()} </li>
+  //   </ol>
+  // </>
+  
+  ,document.getElementById("root")
+);
